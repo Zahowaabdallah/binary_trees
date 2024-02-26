eb182f0a57fac8b04280429182b72c1622fe6ac2
Zahowaabdallah <zahwaabdallah1@gmail.com@example.com>
2024-02-26 10:58:14 -0800

    3-binary_tree_delete.c

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - function use to delete a binary tree
+ * @tree: pointer to the root node of the tree to delete
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree != NULL)
+		tree = NULL;
+}
