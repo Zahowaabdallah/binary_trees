f878fd0552a32f015543d31de49cda385df30daf
Zahowaabdallah <zahwaabdallah1@gmail.com@example.com>
2024-02-27 05:10:47 -0800

    12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,7 +7,11 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	if (tree->left == NULL && tree->right == NULL)
+	if (tree == NULL)
 		return (0);
-	return (1 + binary_tree_leaves(tree->right) + binary_tree_leaves(tree->left));
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
