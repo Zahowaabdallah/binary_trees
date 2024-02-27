da24a2428b77c2271d37bd00d054e1d5b7daf4a3
MohammedAldaif <mohammedaldaif71@gmail.com@example.com>
2024-02-26 22:50:12 -0800

    M

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,9 +1,19 @@
 #include "binary_trees.h"
 
-size_t binary_tree_height(const binary_tree_t *tree) {
-    if(tree == NULL)
-        return 0;
-    if (binary_tree_is_leaf(tree) == 1 || binary_tree_is_root(tree) == 1)
-        return 0;
-    return 1;
+/**
+ * binary_tree_height - function
+ * @tree: pointe
+ * Return: int
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return (1 + (left_height > right_height ? left_height : right_height));
 }
