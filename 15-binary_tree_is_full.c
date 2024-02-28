3c6e2b2424f716288a73abb5c702df06037ce78e
Zahowaabdallah <zahwaabdallah1@gmail.com@example.com>
2024-02-28 06:51:46 -0800

    15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -12,10 +12,12 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 	if (tree != NULL)
 	{
-		if (tree->left != NULL)
-			return (1);
-		if (tree->left != NULL)
+		if (tree->left != NULL && tree->left->left != NULL)
+			return (0);
+		if (tree->left != NULL && tree->right->right != NULL)
+			return (0);
+		if (tree->right != NULL && tree->left->left != NULL)
 			return (0);
 	}
-	return (0);
+	return (1);
 }
