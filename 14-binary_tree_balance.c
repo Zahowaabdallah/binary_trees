c4b4fb3ec1f51f2922034141f8dc7a340b477d23
MohammedAldaif <mohammedaldaif71@gmail.com@example.com>
2024-02-28 22:57:47 -0800

    m

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,10 +1,20 @@
 #include "binary_trees.h"
 #include "9-binary_tree_height.c"
+
+/**
+ * binary_tree_balance - function
+ * @tree: pointer
+ * Return:int
+ */
+
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	int height_of_left_subtree = binary_tree_height(tree->left);
-	int height_of_right_subtree = binary_tree_height(tree->right);
-	return height_of_left_subtree - height_of_right_subtree;
+	size_t height_of_left_subtree = tree->left ==
+	NULL ? (size_t)-1 : binary_tree_height(tree->left);
+	size_t height_of_right_subtree = tree->right == NULL
+	? (size_t)-1 : binary_tree_height(tree->right);
+
+	return (height_of_left_subtree - height_of_right_subtree);
 }
