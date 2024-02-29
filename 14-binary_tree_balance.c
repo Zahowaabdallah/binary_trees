ab1263e237d91c6198ddee68b8cefca41aa94402
MohammedAldaif <mohammedaldaif71@gmail.com@example.com>
2024-02-28 23:03:35 -0800

    m

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -4,17 +4,16 @@
 /**
  * binary_tree_balance - function
  * @tree: pointer
- * Return:int
+ * Return: int
  */
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-	size_t height_of_left_subtree = tree->left ==
-	NULL ? (size_t)-1 : binary_tree_height(tree->left);
-	size_t height_of_right_subtree = tree->right == NULL
-	? (size_t)-1 : binary_tree_height(tree->right);
+    if (tree == NULL)
+        return (0);
 
-	return (int)(height_of_left_subtree - height_of_right_subtree);
+    size_t height_of_left_subtree = (tree->left == NULL) ? (size_t)-1 : binary_tree_height(tree->left);
+    size_t height_of_right_subtree = (tree->right == NULL) ? (size_t)-1 : binary_tree_height(tree->right);
+
+    return (int)(height_of_left_subtree) - (int)(height_of_right_subtree);
 }
